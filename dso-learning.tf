2de7af51c1077936f2954ed5ba3653a96642753d
jeremy.barnes-braingu.com <jeremy.barnes@braingu.com>
2025-10-31 12:40:58 -0400

    updating tf to enable sessionmanager

--- dso-learning.tf
+++ dso-learning.tf
@@ -64,7 +64,7 @@ resource "aws_iam_role" "ec2_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "custom" {
-  name = aws_iam_role.ec2_role.name
+  role = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
