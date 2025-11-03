32f083d7c89f031ae20da4716ea82553fa047548
jeremy.barnes-braingu.com <jeremy.barnes@braingu.com>
2025-11-03 14:28:35 -0500

    trying to get ssm going

--- dso-learning.tf
+++ dso-learning.tf
@@ -2,10 +2,11 @@
 
 ## 1. AWS Provider and Region Configuration
 terraform {
+  required_version = "~> 1.13.3"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.14.1"
     }
   }
 }
